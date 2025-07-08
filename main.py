e518131c70926baf5bc115588b83791715510e69
Narek Hayrapetyan <narek@Narek-Hayrapetyan-MBP.local>
2025-07-08 19:36:25 +0400

    fixing deploying job

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 mock_products = [
     {"id": 1, "name": "Laptop", "price": 1200},
     {"id": 2, "name": "Mouse", "price": 25},
-    {"id": 3, "name": "Keyboard", "price": 70}
+    {"id": 3, "name": "Keyboard", "price": 75}
 ]
 
 @app.get("/products", response_model=List[dict])
